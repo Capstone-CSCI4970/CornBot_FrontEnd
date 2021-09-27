5727cb6f3cdb9c62949d3a1a9b4334deca5c8e5c
Cody <codyroe72@gmail.com>
2021-09-27 18:27:25 -0500

    Added basic user registration / login
    Users can now login/register via frontend by this example.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
-export class AppComponent {
-  title = 'CornBot';
+export class AppComponent implements OnInit {
+
+  register: any;
+  title = 'CornBots';
+  authToken = '';
+
+  constructor(private userService : UserService) {}
+
+  ngOnInit(): void {
+    this.register = {
+      username: '',
+      password: '',
+      email: '',
+    }
+  }
+  
+  registerUser(): void {
+    this.userService.registerUser(this.register).subscribe(
+      resp => {
+        alert('User ' + this.register.username + ' has been created');
+      },
+      error => 
+        console.log(error)
+    );
+  }
+
+  loginUser(): void {
+    this.userService.loginUser(this.register).subscribe (
+      response => {
+        this.authToken = response['token']
+        console.log("This is the authtoken: ", this.authToken)
+      },
+      error =>
+        console.log(error)
+    )
+  }
+
+  // example authentication using tutorials endpoint
+  getTutorials(): void {
+    console.log("Getting tutorials.....\n")
+    this.userService.getTutorials(this.authToken).subscribe (
+      response => {
+        console.log(response)
+      },
+      error =>
+        console.log(error)
+    )
+  }
+
 }
