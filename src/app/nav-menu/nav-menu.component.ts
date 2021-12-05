ffbf26fa69852e344c570d791b1a76053039b0ed
aBurbach1 <burbach.aaron@gmail.com>
2021-12-05 15:13:44 -0600

    truncated number displays for activities and leaderboard, added logout button

--- src/app/nav-menu/nav-menu.component.ts
+++ src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-menu',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  /**logs out user by clearing username, uid, and authtoken 
+   * then redirects to login screen
+   */
+  logout() {
+    localStorage.clear();
+    console.log("logged out");
+    this.router.navigate(['/login']);    
   }
 
 }
